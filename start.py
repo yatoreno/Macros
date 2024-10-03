70d338fc7a921dbef425f0d83152e31e7e5a7d96
yatoreno <85769618+yatoreno@users.noreply.github.com>
2024-10-03 18:12:58 +0300

    Add files via upload

--- start.py
+++ start.py
@@ -1,12 +1,18 @@
 import sys
 
+from PySide6 import QtGui
 from PySide6.QtWidgets import QApplication
 
+from my_code.FileWork import File
 from my_code.main_window import MainWindow
 
 # Запуск основного окна и всего кода
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    app.setWindowIcon(QtGui.QIcon(File.resource_path('app.ico')))
+    print(File.resource_path('app.ico'))
+
     w = MainWindow()
     w.show()
     sys.exit(app.exec())
